640ce6d2bcd1b34c58d32ec46665bc36fc94b844
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-11-10 06:29:59 +0000

    change to interactive input

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 #let's putss list of all students into an array
+=begin
 students =[
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -12,6 +13,25 @@ students =[
   {name: "Joffrey Baretheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
+def input_student
+  puts "Please enter the names of the students"
+  puts "To finish, press enter twice"
+  #create empty array
+  student = []
+  #gets first name
+  name = gets.chomp
+  #while code is not emoty, repeat code
+  while !name.empty? do
+    #add name to the array as a hash
+    student << {name: name, cohort: :november}
+    puts "Now we have #{student.count} students"
+    #get another name from input
+    name = gets.chomp
+  end
+  #return the student array
+  student
+end
 def print_header
   puts "Welcome to Villian academy"
   puts "__________________________"
@@ -25,6 +45,8 @@ def print_footer(names)
   puts "Overall we have #{names.count} great students"
 end
 #nothing happens untill we call this method
+students = input_student
+#nothing happens in the code till this point
 print_header
 print(students)
 print_footer(students)
