ee167327fbeea0ecb9be2fe14c0fdd4010aced29
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-11-10 13:30:28 +0000

    refacturing code to methods

--- directory.rb
+++ directory.rb
@@ -14,58 +14,62 @@ students =[
   {name: "Norman Bates", cohort: :november}
 ]
 =end
+@student = []
 def input_student
   puts "Please enter the names of the students"
   puts "To finish, press enter twice"
   #create empty array
-  student = []
   #gets first name
   name = gets.chomp
   #while code is not emoty, repeat code
   while !name.empty? do
     #add name to the array as a hash
-    student << {name: name, cohort: :november}
-    puts "Now we have #{student.count} students"
+    @student << {name: name, cohort: :november}
+    puts "Now we have #{@student.count} students"
     #get another name from input
     name = gets.chomp
   end
   #return the student array
-  student
 end
 def print_header
   puts "Welcome to Villian academy"
   puts "__________________________"
 end
-def print(names)
-  names.each do |student|
+def print
+  @student.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall we have #{names.count} great students"
+def print_footer
+  puts "Overall we have #{@student.count} great students"
 end
-def interactive_menu
-  student =[]
-  loop do
+def print_menu
   #print the menu and ask the user what to do
   puts "1. Input the student"
   puts "2. Show the students"
   puts "9. Exit"
-  #read the input and save to a variable
-  selection = gets.chomp
-  #do what the user has asked
+end
+def show_student
+  print_header
+  print
+  print_footer
+end
+def process(selection)
   case selection
     when "1"
       student = input_student
     when "2"
-      print_header
-      print(student)
-      print_footer(student)
+      show_student
     when "9"
       exit
     else
       puts "I don't know what you meant, try again"
-    end
+  end
+end
+def interactive_menu
+  loop do
+  print_menu
+  process(gets.chomp)
   end
 end
 
